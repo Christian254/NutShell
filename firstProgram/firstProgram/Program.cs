99102a06f498685bcc859acc8aec55620e9128be
Christian254 <christianfuentes254@gmail.com>
2020-03-06 18:46:43 -0600

    First program

---
+++ firstProgram/firstProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+            Console.WriteLine(FeetToInches(12));
+            Console.WriteLine(FeetToInches(100));
+        }
+
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
