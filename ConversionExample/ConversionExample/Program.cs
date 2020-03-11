ec717cacf85f7cc428eb0f53cd457395b514004f
Christian254 <christianfuentes254@gmail.com>
2020-03-10 22:02:12 -0600

    Conversion

---
+++ ConversionExample/ConversionExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConversionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Christian Fuentes 
+            // Conversions Implicits and Explicits 
+            //both kinds of conversion are prohibited
+            int x = 12345; // int is a 32-bit integer
+            long y = x; // Implicit conversion to 64-bit integer
+            short z = (short)x; // Explicit conversion to 16-bit integer
+        }
+    }
+}
