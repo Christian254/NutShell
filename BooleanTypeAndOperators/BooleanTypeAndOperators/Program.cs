dbd25c629154d2ad2dbe70a4186eb09f48c2f09b
Christian254 <christianfuentes254@gmail.com>
2020-03-16 22:15:22 -0600

    practicing

---
+++ BooleanTypeAndOperators/BooleanTypeAndOperators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BooleanTypeAndOperators
+{
+    class Program
+    {
+        //Page 37 - 38 - 39
+        public class Dude
+        {
+            public string Name;
+            public Dude(string n) { Name = n; }
+        }
+        static void Main(string[] args)
+        {
+
+            //Equality and Comparison Operators
+            int x = 1;
+            int y = 2;
+            int z = 1;
+            Console.WriteLine(x == y); // False
+
+            Console.WriteLine(x == z); // True
+
+            //For references types 
+            Dude d1 = new Dude("John");
+            Dude d2 = new Dude("John");
+            Console.WriteLine(d1 == d2); // False
+            Dude d3 = d1;
+            Console.WriteLine(d1 == d3); // True
+
+            //Conditional operators
+            bool useUmbrella = UseUmbrella(true, false, false);
+
+            //Ternary Operator
+            Console.WriteLine(useUmbrella ? "Yes, you should use umbrella": "No");
+        }
+        static bool UseUmbrella(bool rainy, bool sunny, bool windy)
+        {
+            return !windy && (rainy || sunny);
+        }
+    }
+}
---
+++ StringsAndCharacters/StringsAndCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringsAndCharacters
+{
+    class Program
+    {
+        //Page 39 - 40
+        //String and Characters
+        static void Main(string[] args)
+        {
+            char c = 'A'; // Simple character
+
+            //Scape Secuences
+            //char newLine = '\n';
+            //char backSlash = '\\';
+
+            //The \u(or \x) escape sequence lets you specify any Unicode character via its fourdigit hexadecimal code:
+            char copyrightSymbol = '\u00A9';
+            char omegaSymbol = '\u03A9';
+            char newLine = '\u000A';
+
+        }
+    }
+}
