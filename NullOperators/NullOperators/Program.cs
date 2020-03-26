e61d351e7b99cfbb2b9a03fa44786794310d2169
Christian254 <christianfuentes254@gmail.com>
2020-03-26 09:40:59 -0600

    Practicing

---
+++ NullOperators/NullOperators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NullOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PAge 60
+            /* C# provides two operators to make it easier to work with nulls: the null coalescing
+            operator and the null-conditional operator. */
+
+            //Null Coalescing Operator ??
+            string s1 = null;
+            //if is null assigns nothing to S2"
+            string s2 = s1 ?? "nothing"; // s2 evaluates to "nothing"
+            Console.WriteLine(s2);
+
+
+            //Page 61
+            //Null conditional Operator ?.
+            System.Text.StringBuilder sb = null;
+            string s = sb?.ToString(); // No error; s instead evaluates to null
+            Console.WriteLine(s);
+            //The last line is equivalent to:
+            //string s = (sb == null ? null : sb.ToString());
+
+            System.Text.StringBuilder sb1 = null;
+            string s3 = sb1?.ToString().ToUpper(); // s evaluates to null without error
+
+            //The final expression must be capable of accepting a null. 
+            //System.Text.StringBuilder sb = null;
+            // int length = sb?.ToString().Length; // Illegal : int cannot be null
+            //int? length = sb?.ToString().Length; // OK : int? can be null
+
+            System.Text.StringBuilder sb2 = null;
+            string s4 = sb2?.ToString() ?? "nothing"; // s evaluates to "nothing"
+            Console.WriteLine($"s4: {s4}");
+        }
+    }
+}
---
+++ Statements/Statements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+
+namespace Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Page 62
+            //Declaration Statements
+            //Declaring multiple variables of the same type in a comma-separated list
+            string someWord = "rosebud";
+            int someNumber = 42;
+            bool rich = true, famous = false;
+
+            //Constant declaration is like a variable declaration, except that it cannot
+            //be changed after it has been declared;
+            const double c = 2.99792458E08;
+            //c += 10; // Compile-time Error
+
+            //Loxal variables
+            //int x;
+            //{
+            //    int y;
+            //    //int x; // Error - x already defined
+            //}
+            //{
+            //    int y; // OK - y not in scope
+            //    Console.WriteLine(y = 10);
+            //}
+            //Console.Write(y); // Error - y is out of scope
+
+            //Page 63
+            //Expression Statements
+            // Declare variables with declaration statements:
+            string s;
+            int x, y;
+            StringBuilder sb;
+            // Expression statements
+            x = 1 + 2; // Assignment expression
+            x++; // Increment expression
+            Console.WriteLine(x);
+            y = Math.Max(x, 5); // Assignment expression
+            Console.WriteLine(y); // Method call expression
+            sb = new StringBuilder(); // Assignment expression
+            new StringBuilder(); // Object instantiation expression
+            new StringBuilder(); // Legal, but useless
+            new string('c', 3); // Legal, but useless
+            Console.WriteLine(x.Equals(y)); // Legal, but useless
+        }
+    }
+}
